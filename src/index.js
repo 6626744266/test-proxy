9365216afa8ddca1dbb3f738b97d4b515c61548b
NamPhan <nam230687@gmail.com>
2022-11-12 18:55:39 +0700

    fgh

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express');
+const app = express();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app.use(express.json());
+app.use(express.static("."))
+
+app.listen(8080, () => { })
+
+
+
+
+
+app.get('/', (req, res) => {
+    let findProxyForURL = () => {
+        return "PROXY 192.168.82.185:10809"
+    }
+
+    res.send(findProxyForURL())
+})
